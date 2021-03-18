4eae3144d25de83748333013cba423ba70e80a3b
Stanislas Ormières <stan@stormier.ninja>
2021-03-18 21:22:40 +0100

    :sparkles: Add router and 2 routes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
 </template>
-
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
 
 <style>
 #app {
