710f167b4ce524a29aa38d299037fcec6d824e2f
Stanislas Ormières <stan@stormier.ninja>
2021-03-24 17:46:41 +0100

    :wrench: Add Jest and set it up

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  env: {
+    test: {
+      plugins: [
+        'babel-plugin-transform-import-meta',
+      ],
+    },
+  },
+}
